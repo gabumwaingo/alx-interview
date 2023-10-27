253004d7f4f0c0a4c8153e830be7c1beb633bbe9
gabumwaingo <gabumwaingo@gmail.com>
2023-10-26 19:45:26 -0700

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,6 @@ def validUTF8(data):
     bytes_to_follow = 0
 
     for num in data:
-        # Check if it's a starting byte
         if bytes_to_follow == 0:
             if (num >> 7) == 0b0:
                 bytes_to_follow = 0
@@ -19,10 +18,8 @@ def validUTF8(data):
             else:
                 return False
         else:
-            # Check if it's a following byte
             if (num >> 6) != 0b10:
                 return False
             bytes_to_follow -= 1
 
-    # If bytes_to_follow is zero at the end, it's a valid UTF-8 encoding
     return bytes_to_follow == 0
